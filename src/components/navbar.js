ad054e264b6bc73632a8e3f6b543039ee44561af
Yoseph <yoseph_hasson@yahoo.com>
2020-08-20 21:59:58 -0400

    removed href causing warning

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,20 +7,20 @@ export default class Nav extends React.Component {
   render() {    
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#"><Link to="/public">Public Page</Link> <span class="sr-only">(current)</span></a>
+                    <a class="nav-link"><Link to="/public">Public Page</Link> <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><Link to="/protected">Protected Page</Link></a>
+                    <a class="nav-link"><Link to="/protected">Protected Page</Link></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"></a>
+                    <a class="nav-link"></a>
                 </li>
                 </ul>
             </div>
